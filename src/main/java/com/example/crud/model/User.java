82a6501e8b19bea2db7e0a10533ce22a989a4104
Kirill Krainov-Timanovsky <79582042+kirillkrainov95@users.noreply.github.com>
2024-01-25 00:01:25 +0300

    crud application with no tests

---
+++ src/main/java/com/example/crud/model/User.java
@@ -0,0 +1,50 @@
+package com.example.crud.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "UserTest")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "age")
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
